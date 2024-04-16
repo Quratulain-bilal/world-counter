4ec2a5e50ab90db6f3e604f566cdb3b584b7139b
Quratulain-bilal <164326821+Quratulain-bilal@users.noreply.github.com>
2024-04-17 00:16:50 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+//import inquirer
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "enter your sentence to count the word:"
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(`your sentence word count is ${words.length}`);
